00c482bceb95a78bc76fac1ea65590eff07889b0
David\David Lawrence <davidxlawrence@gmail.com>
2016-12-22 20:00:02 -0800

    basic out of bounds detector + reset of player position

---
+++ Monkey Ball/Assets/Code/PlayerFallOutDetector.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFallOutDetector : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject player;
+
+	[SerializeField]
+	private Transform spawnPoint;
+
+	void OnTriggerEnter(Collider c) {
+		if (c.gameObject == player) {
+			player.transform.position = spawnPoint.position;
+			player.transform.rotation = spawnPoint.rotation;
+		}
+	}
+}
