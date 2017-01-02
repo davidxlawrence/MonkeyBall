e4f9418f0b447559b7daefae575a8d0187313619
David\David Lawrence <davidxlawrence@gmail.com>
2017-01-02 05:01:27 -0800

    adding a simple warp portal obstacle to the game

---
+++ Monkey Ball/Assets/Code/WarpGate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WarpGate : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject player;
+	
+	[SerializeField]
+	private GameObject targetGate;
+	
+	void OnTriggerEnter(Collider c) {
+		if (c.gameObject == player) {
+			player.transform.position = targetGate.transform.position + targetGate.transform.forward;
+		}
+	}
+}
